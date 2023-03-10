b6520a152caaa547c295e2c7a3d05241093e044c
lucas-laza <laza.lucas15@gmail.com>
2023-03-10 16:40:08 +0100

    Arborescence du projet, ajout des app.js et server.js inspirés du cours (+launch.json)

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+// const path = require("path");
+// const userRoute = require("./route/user.route");
+// const authRoute = require("./route/auth.route");
+
+// const {connect} = require("./model/connection");
+
+// connect();
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+// app.use('/user', userRoute);
+// app.use('/auth', authRoute);
+
+module.exports = app;
