9ca4216ff9e71e7db0908da76c802be85eaf42cb
Matt <mboureau59@gmail.com>
2023-03-30 16:27:35 +0200

    Change on "post.controller.js"

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,17 +1,17 @@
 const Post = require("./../model/post.model")
 
 exports.create = (req,res,next) => {
-
+  Post.create({});
+  res.status(201).json({ message: "Votre post à été créé" });
 }
 
 exports.getAll = (req,res,next) => {
-  
 }
 
 exports.edit = (req,res,next) => {
-  
+  res.status(201).json({ message: "Post mis à jour !" });
 }
 
 exports.delete = (req,res,next) => {
-  
+  res.status(201).json({ message: "Post supprimé" });
 }
