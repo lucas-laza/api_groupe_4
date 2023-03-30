0c9efb683c82eba758af357ec92c6cb313d8b617
Matt <mboureau59@gmail.com>
2023-03-30 16:14:14 +0200

    Maj post.controller : add function

---
+++ controller/post.controller.js
@@ -0,0 +1,17 @@
+const Post = require("./../model/post.model")
+
+exports.create = (req,res,next) => {
+
+}
+
+exports.getAll = (req,res,next) => {
+  
+}
+
+exports.edit = (req,res,next) => {
+  
+}
+
+exports.delete = (req,res,next) => {
+  
+}
