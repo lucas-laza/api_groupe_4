65ff974fd879bf0a8205ffa507460274bd53701e
lucas-laza <laza.lucas15@gmail.com>
2023-04-02 21:56:00 +0200

    pas de fichier data

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,11 +1,34 @@
 const Post = require("./../model/post.model")
+const Comment = require('./../model/comment.model')
 
-exports.create = (req,res,next) => {
-  Post.create({});
-  res.status(201).json({ message: "Votre post à été créé" });
+exports.create = async (req, res, next) => {
+  const post = req.body.post;
+  Post.create({
+    ...post,
+    // userId: req.token.id
+  });
+  res.status(201).json({ message: "Post créé" });
+}
+
+exports.createComment = async (req, res, next) => {
+  const comment = req.body.comment;
+    const postId = req.params.id;
+    Comment.create({
+        ...comment,
+        postId: postId
+        // userId: req.token.id
+    });
+    res.status(201).json({ message: "Commentaire créé" });
 }
 
 exports.getAll = (req,res,next) => {
+  Post.find()
+    .then(postList => {
+        res.status(200).json(postList);
+    })
+    .catch(error => {
+        res.status(500).json(error);    
+    })
 }
 
 exports.edit = (req,res,next) => {
