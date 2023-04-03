4f793dee5bb33c05538ae80a88bca03ab709603c
lucas-laza <laza.lucas15@gmail.com>
2023-04-03 11:27:06 +0200

    UserId en relation, Enregistrement du UserId lors de la création; vérif userId lors de la suppr

--- model/comment.model.js
+++ model/comment.model.js
@@ -3,10 +3,8 @@ const { Schema } = mongoose;
 
 const Comment = new Schema ({
     content: String,
-    userId: String,
-    // userId: {type: String, ref: "User"}
-    postId: String,
-    // postId: {type: String, ref: "Post"}
+    userId: {type: String, ref: "User"},
+    postId: {type: String, ref: "Post"}
 })
 
 module.exports = mongoose.model("Comment", Comment);
